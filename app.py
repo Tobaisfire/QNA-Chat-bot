99e833c20464a987c059235f54cb70c27b47d314
Keval Sing Saud <67000746+Tobaisfire@users.noreply.github.com>
2023-06-10 16:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,18 +14,10 @@ def chatbot(prompt2):
     result = chatqa(prompt2)
 
     return result
-# def imgen(prompt3):
-    
-
-
-#     model_path = r"D:\keval\study\Projects\Chatbot\Trained_model_using_LoRA.bin"
-#     pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16)
-#     pipe.unet.load_attn_procs(model_path)
-#     # pipe.to("cuda")
 
     
-#     image = pipe(prompt3, num_inference_steps=30, guidance_scale=8.5).images[0]
-#     return image.show()
+
+
 
 
 
@@ -49,17 +41,7 @@ with gr.Blocks() as demo:
         sr_btn2 = gr.Button("chat")
         
         sr_btn2.click(fn=chatbot, inputs=prompt2, outputs=output2)
-    
-    #  with gr.Tab("Prop2IMG"):
-    #     prompt3= gr.Textbox(label="Prompt")
-      
- 
-    
-    
-    #     output3 = gr.Textbox(label="Output")
-    #     sr_btn3 = gr.Button("Imgen")
-        
-    #     sr_btn3.click(fn=imgen, inputs=prompt3, outputs=output3)
+
        
 
 
